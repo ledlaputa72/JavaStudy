51238ae09843750c4a6f77f2fbc9c8b0e037c10b
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-13 15:16:05 +0900

    up

--- FirstProject/src/java005/Animal_Bird.java
+++ FirstProject/src/java005/Animal_Bird.java
@@ -1,14 +1,37 @@
 package java005;
 
 class Animal{
+	String noon;
+	public Animal() { //Animal 생성자1
+		System.out.println("난 동물이야!");
+	}
+	
+	public Animal(int soo) { //Animal 생성자2
+		System.out.println("나는 눈이 "+soo+"개야.");
+	}
+
 	public void soomshim() {
 		System.out.println("SoomShim");
 	}
 }
 
 class Bird extends Animal{
+	int soo;
+	
+	public Bird(String noon, int soo) { //Bird 생성자
+		super(soo);//Animal 생성자 2를 찾아감 
+		this.noon=noon;
+		this.soo=soo;
+		System.out.println("자녀가 객체에다 값 넣기 끝");
+	}
+	
+	public Bird() {
+		
+	}
+
 	public void singing() {
 		System.out.println("Singing");
+		System.out.println("난 새야");
 	}
 }
 public class Animal_Bird {
@@ -20,6 +43,7 @@ public class Animal_Bird {
 		Bird b1=new Bird();
 		b1.soomshim();
 		b1.singing();
+		Bird b2=new Bird("eyes",2);
 	}
 
 }
--- FirstProject/src/java005/InheritanceStudy.java
+++ FirstProject/src/java005/InheritanceStudy.java
@@ -2,9 +2,11 @@ package java005;
 //상속 
 class Boomo { // 부모 클래스
 	String juso; //부모의 속성(필드), 접근지정자의 Default(앞에 public 같은게 없음)  
-	
-	public Boomo() {
+	public Boomo() { //생성자1 오버로딩
 		System.out.println("나 나이는 젊어 100살 아래야");
+	}
+	public Boomo(int na2) { //생성자2 오버로딩
+		System.out.println("에이 그래도"+na2+"살 보다는 말지~");
 	}
 	public void don() { // 부모의 메소드
 		System.out.println("나 재산 많아~");
@@ -14,10 +16,13 @@ class Boomo { // 부모 클래스
 class Janyu extends Boomo{ //자식 클래스
 	protected int na2; // 자식의 속성(필드), 상속된 것들 끼리만 통할 수 있는 접근지정자 : protected
 	
-	  public Janyu(String juso, int na2) { //생성자1
+	public Janyu(String juso, int na2) { //생성자1
+	  super(na2); //명시적으로 부모를 찾는 방법, Boomo 생성자2로 찾아감 
+	  //super() //-> Boomo생성자 1로 찾아감
 	  this.na2=na2;
 	  this.juso=juso; //부모의 juso를 상속 받아옴
-	  }
+	  System.out.println("나는 자녀 생성자");
+	}
 	 
 	public Janyu() { //생성자2
 		
