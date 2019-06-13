37f19aa2693f63f87975a81dfcb8e378782433e5
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-13 14:40:36 +0900

    update

--- FirstProject/src/java005/InheritanceStudy.java
+++ FirstProject/src/java005/InheritanceStudy.java
@@ -1,21 +1,25 @@
 package java005;
 //상속 
 class Boomo { // 부모 클래스
-	String juso;
-	public void don() {
+	String juso; //부모의 속성(필드), 접근지정자의 Default(앞에 public 같은게 없음)  
+	
+	public Boomo() {
+		System.out.println("나 나이는 젊어 100살 아래야");
+	}
+	public void don() { // 부모의 메소드
 		System.out.println("나 재산 많아~");
 	}
 }
 
 class Janyu extends Boomo{ //자식 클래스
-	int na2;
+	protected int na2; // 자식의 속성(필드), 상속된 것들 끼리만 통할 수 있는 접근지정자 : protected
 	
-	public Janyu(String juso, int na2) {
-		this.na2=na2;
-		this.juso=juso;
-	}
-	
-	public Janyu() {
+	  public Janyu(String juso, int na2) { //생성자1
+	  this.na2=na2;
+	  this.juso=juso; //부모의 juso를 상속 받아옴
+	  }
+	 
+	public Janyu() { //생성자2
 		
 	}
 	
@@ -33,6 +37,9 @@ public class InheritanceStudy {
 		Janyu j1=new Janyu();
 		j1.don(); // Boomo 클래스에서 상속 받은 내용
 		j1.car();
+		Janyu j2=new Janyu("종로",30);
+		
+		System.out.println(j2.na2+"\t"+j2.juso);
 	}
 
 }
