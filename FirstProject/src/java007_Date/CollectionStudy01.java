b232c24f983ebbba6beb88612dafd2aa2e27f1ff
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-19 15:12:42 +0900

    add

--- FirstProject/src/java007_Date/CollectionStudy01.java
+++ FirstProject/src/java007_Date/CollectionStudy01.java
@@ -1,9 +1,17 @@
 package java007_Date;
 
+import java.util.Vector;
+
 public class CollectionStudy01 {
 
 	public static void main(String[] args) {
 		
+		Vector v7=new Vector(3);
+		
+		v7.add(new Integer(100));
+		v7.add(new Integer(20));
+		v7.add(new Integer(300));
+		v7.add(new Integer(700));
 		
 	}
 
