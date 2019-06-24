9fa6f21da88cf9b59f394fd93e3c528a999d4f8b
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-24 14:09:55 +0900

    add

--- FirstProject/src/java010_outputStream/OutPutStreamEx01.java
+++ FirstProject/src/java010_outputStream/OutPutStreamEx01.java
@@ -10,6 +10,18 @@ public class OutPutStreamEx01 {
 		FileOutputStream fo1=new FileOutputStream("E:/dd.txt", true);
 		//메모리에 있는걸 파일로 저장하는 것 
 		
+		System.out.println("글자 입력한다. ");
+		while (true) {
+			int cnt=System.in.available();
+			if (cnt>0) {
+				byte[] b1=new byte[cnt];
+				int bada = System.in.read(b1);
+				if(bada==-1) break;
+				fo1.write(b1,0,bada);
+				System.out.println("수록중");
+			}
+		}
+		fo1.close();
 	}
 
 }
