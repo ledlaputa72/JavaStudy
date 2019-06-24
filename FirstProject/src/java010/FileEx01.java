dd4cc2d0b1426cf6eef50f4f5f57a2edcd95de17
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-24 13:35:14 +0900

    add

--- FirstProject/src/java010/FileEx01.java
+++ FirstProject/src/java010/FileEx01.java
@@ -1,10 +1,29 @@
 package java010;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileEx01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		//파일 오픈 inputStream
+		FileInputStream f1=new FileInputStream("E:/aa.txt");
+		
+		//파일 읽고 쓰기  ouputStream
+		int cnt;
+		while ((cnt = f1.available())>0) { //읽어올수 있는 바이트 수가0바이트 이상이면
+			byte[] b1=new byte[cnt];
+			int bada = f1.read(b1); //b1크기만큼 읽기
+			//sysout(bada);
+			if(bada==-1) //bada는 더이상 읽을게 없으면 -1값을 갖는다. 
+				break;
+			String str1=new String(b1); //char->String으로 바꿈 
+			//b1[](배열)에 들어가있는 char형태가 String으로 바뀌게 된다.  
+			System.out.println(str1);
+			System.out.println("잠깐만요~ 출력하고 가실께요");
+		}
+		f1.close();
 	}
 
 }
