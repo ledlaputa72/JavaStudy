fc4a5d99300b5cafdc6ecdd179cdc9614e76005d
network5096 <46048095+network5096@users.noreply.github.com>
2019-06-27 09:59:12 +0900

    add

--- FirstProject/src/toyStore2/Caculate.java
+++ FirstProject/src/toyStore2/Caculate.java
@@ -1,5 +1,17 @@
 package toyStore2;
 
-public class Caculate {
+public class Caculate extends Toys {
 
+	public Caculate() {
+		super();
+	}
+
+	public Caculate(String tID, String tName, String tType, int tPrice) {
+		super(tID, tName, tType, tPrice);
+	}
+
+	public void addToys(String tID, String tName, String tType, int tPrice) {
+		
+	}
+	
 }
