50839e990c19f458b2d97ac8575b5b037191335a
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-01 15:13:25 +0900

    add

--- FirstProject/src/java_GUI01/ButtonClass.java
+++ FirstProject/src/java_GUI01/ButtonClass.java
@@ -1,46 +1,50 @@
 package java_GUI01;
 
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ButtonClass extends Frame {
+class ButtonClass2 extends Frame implements ActionListener{ //창문 만들기 위해서 필수 
 	
-	public ButtonClass() {
+	private Button b1=new Button("눌러봐"); //속성역할
+	private Label la1=new Label("여기에 나온다."); //속성역할 
+	
+	public ButtonClass2() { //생성자
 		
-		//창문 닫기
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e1) {
-				dispose(); //개발자가 직접 메모리의 내용을 지우는 명령
-				System.exit(0);
-			}
-		});
+		this.setLayout(null); //Layout은 사용자 정의로 
+		
+		//버튼///////////////////////////////
+		b1.setSize(100, 70); //버튼 크기
+		b1.setLocation(200, 300); //버튼 내부위치
+		this.add(b1); //버튼추가 
+		b1.addActionListener(this);//버튼에 이벤트 발생 시 이벤트 핸들러
+		//버튼만 이벤트 가능하다는 뜻 /b1.에만 달려있으므로 
+		
+		//라벨///////////////////////////////
+		la1.setSize(100, 70);
+		la1.setLocation(200, 150);//라벨 내부위치
+		this.add(la1);//라벨추가
+		
+		//메인 프레임//////////////////////////
+		this.setSize(500, 500);//프레임 크기
+		this.setVisible(true);
+		
+	}//생성자 end
+
+	//이벤트 처리할 내용을 적는 자리 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==b1) { //버튼이 눌러졌으면 처리 (이벤트 핸들러)
+			la1.setText("안녕~~ 자바");
+		}
 	}
-	
+}//ButtonClass2 end
+
+public class ButtonClass {
+
 	public static void main(String[] args) {
-		//메인 프레임//////////////////////////////////
-		ButtonClass bc1=new ButtonClass(); 
-		bc1.setSize(500, 500);
-		//Panel p1=new Panel(); //패널
-		//p1.setSize(300,300); //패널 크기 
-		bc1.setTitle("Button이야");
-		bc1.setLayout(null); //No Layout 
-		bc1.setVisible(true);
-		
-		//Button 버튼///////////////////////////////
-		Button b1=new Button("눌러봐"); //내장 클래스, 사용자 정의 클래스 
-		b1.setSize(200,200);
-		b1.setLocation(100, 100);
-		//Button b1=new Button();
-		//b1.setLabel("눌러봐");
-		
-		//p1.add(b1);
-		b1.setVisible(true);
-		bc1.add(b1); //버튼 붙이기 
-		
-		System.out.println(b1.getLabel());
-		//버튼의 title dos창에 인쇄하기 
-	}
 
-}
+		ButtonClass2 bee1=new ButtonClass2(); //새로만드는 클래스,two class 방식
+		
+	}
+}//main class end
