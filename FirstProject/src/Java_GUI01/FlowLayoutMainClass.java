1dac5948c7383702b552e1c06ab411cdafb2721f
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-01 13:05:04 +0900

    add

--- FirstProject/src/Java_GUI01/FlowLayoutMainClass.java
+++ FirstProject/src/Java_GUI01/FlowLayoutMainClass.java
@@ -1,15 +1,21 @@
 package Java_GUI01;
 
+import java.awt.FlowLayout;
 import java.awt.Frame;
 
 class FlowLayoutMainClass extends Frame{
 	
 	public FlowLayoutMainClass() { //생성자
+		this.setLayout(new FlowLayout()); //this 자기자신을 참조하는 객체, FlowLayoutMainClass를 참조
+		//가로 중앙정렬, 상하좌우 5픽셀간격
+		//즉, 좌에서 우로 배치, 다시 다음줄 좌에서 우로 배치
+		//같은 줄에서는 가운데 정렬
+		//실행한 윈도우를 크기를 줄이거나 늘렸을때 자동적용 
 		
+		//setLayout(null)은 개발자 임의대로 
 	}
 
 	public static void main(String[] args) {
-		//setLayout(null); 참고
 		//FlowLayoutMainClass.java
 		FlowLayoutMainClass flc1=new FlowLayoutMainClass(); //one class방식
 		//객체 생성 후 제일먼저 가동 >> 생성자 
