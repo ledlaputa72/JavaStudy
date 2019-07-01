be0c4f9188dc890a6bd8c2e864cf23259f1269b6
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-01 12:58:12 +0900

    add

--- FirstProject/src/Java_GUI01/FlowLayoutMainClass.java
+++ FirstProject/src/Java_GUI01/FlowLayoutMainClass.java
@@ -2,12 +2,20 @@ package Java_GUI01;
 
 import java.awt.Frame;
 
-public class FlowLayoutMainClass extends Frame{
+class FlowLayoutMainClass extends Frame{
+	
+	public FlowLayoutMainClass() {
+		
+	}
 
 	public static void main(String[] args) {
 		//setLayout(null); 찹고
 		//FlowLayoutMainClass.java
 		FlowLayoutMainClass flc1=new FlowLayoutMainClass(); //one class방식
+		//객체 생성 후 제일먼저 가동 >> 생성자 
+		//생성자를 잘 사용하는 것이 중요
+		//원래 디폴트생성자는 자동으로 만들어져 있다.
+		//디폴드 생성자는 처리할 내용이 없으므로 여기서는  개발자가 직접 만듦
 	}
 
 }
