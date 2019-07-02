af1ef75aaa2ef8e937efa86c3c7d23be42697234
ledlaputa <ledlaputa@DESKTOP-2QT66QE>
2019-07-03 00:49:01 +0900

    add

--- FirstProject/src/homestudy/BookStoreGUI.java
+++ FirstProject/src/homestudy/BookStoreGUI.java
@@ -8,13 +8,15 @@ class BookStore extends Frame implements ActionListener{
 	//Components /////////////////////////////////////////////
 	private Frame f1=new Frame();
 	
-	private Panel p1=new Panel();
-	private Panel p2=new Panel();
-	private Panel p3=new Panel();
+	private Panel p0=new Panel(); //상위 패널
+	private Panel p1=new Panel(); //타이틀 패널
+	private Panel p2=new Panel(); //버튼 패널
+	private Panel p3=new Panel(); //텍스트 패널
 	
 	private BorderLayout b1= new BorderLayout();
-	private GridLayout g1=new GridLayout(1,2);
-	private GridLayout g2=new GridLayout(1,4);
+	private GridLayout g0=new GridLayout(2,1); //상위 패널용
+	private GridLayout g1=new GridLayout(1,1); //타이틀 패널용
+	private GridLayout g2=new GridLayout(1,4); //버튼 패널용
 	
 	private Label lb1=new Label("북스토어 관리 프로그램");
 	private Label lb2=new Label();
@@ -38,32 +40,48 @@ class BookStore extends Frame implements ActionListener{
 		ta1.setSize(300, 300);
 		ta1.setVisible(true);
 		
+		
 		//버튼
+		bt1.setFont(new Font("굴림",Font.BOLD,18));
+		bt2.setFont(new Font("굴림",Font.BOLD,18));
+		bt3.setFont(new Font("굴림",Font.BOLD,18));
+		bt4.setFont(new Font("굴림",Font.BOLD,18));
+		
+		bt1.setBackground(new Color(0,255,255));
+		bt2.setBackground(new Color(0,255,255));
+		bt3.setBackground(new Color(0,255,255));
+		bt4.setBackground(new Color(0,255,255));
+		
 		bt1.addActionListener(this);
 		bt2.addActionListener(this);
 		bt3.addActionListener(this);
 		bt4.addActionListener(this);
 		
-		//패널 버튼 
+		//North 패널 
+		p0.setLayout(g0);
+		p0.add(p1);
+		p0.add(p2);
+	
+		//패널 타이틀 - North 
 		p1.setLayout(g1);
-		p1.add(bt1);
-		p1.add(bt2);
-		p1.add(bt3);
-		p1.add(bt4);
+		p1.setBackground(new Color(255,255,0));
+		p1.add(lb1);
 		
-		//패널 타이틀 
+		//버튼  패널 - North
 		p2.setLayout(g2);
-		p2.add(lb1);
+		p2.add(bt1);
+		p2.add(bt2);
+		p2.add(bt3);
+		p2.add(bt4);
 		
 		//패널 텍스트 
-		p3.setLayout(g2);
+		p3.setLayout(g1);
 		p3.add(ta1);
 		
 		//메인 프레임
 		f1.setLayout(b1);
-		f1.add("South",p1);
-		f1.add("North",p2);
-		f1.add("Center",p3);
+		f1.add("North",p0);
+//		f1.add(p3);
 		f1.setSize(frameWeith, frameHeigth);
 		f1.setVisible(true);
 		f1.setResizable(false);
