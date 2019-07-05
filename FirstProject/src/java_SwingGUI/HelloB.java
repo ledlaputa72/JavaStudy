d3b8b1219b615900b1ba802c1d3165bd5ea843e4
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-05 15:12:40 +0900

    add

--- FirstProject/src/java_SwingGUI/HelloB.java
+++ FirstProject/src/java_SwingGUI/HelloB.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HelloB extends JFrame {
-
+	
 	private JPanel contentPane;
+	private JLabel lblNewLabel = new JLabel(""); //lblNewLabel를 전역 변수로 변경
 
 	/**
 	 * Launch the application.
@@ -35,8 +41,22 @@ public class HelloB extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnImsi = new JButton("\uB20C\uB7EC\uBD10"); //한글 유니코드
+		btnImsi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//라벨 객체.메소드() 
+				lblNewLabel.setText("안녕 자바");
+			}
+		});
+		btnImsi.setBounds(164, 188, 97, 23);
+		contentPane.add(btnImsi);
+		
+//		JLabel lblNewLabel = new JLabel(""); //lblNewLabel가 지역 변수 -> 전역 변수로 변경 필요 18번 라인트로 이동
+		lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 25));
+		lblNewLabel.setBounds(115, 36, 199, 54);
+		contentPane.add(lblNewLabel);
 	}
-
 }
