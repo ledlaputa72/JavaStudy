53e5f080ad57ecb75da6afc1df14bbad3177d962
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-08 14:54:16 +0900

    add

--- FirstProject/src/java_SwingGUI/JCheckBoxMainClass.java
+++ FirstProject/src/java_SwingGUI/JCheckBoxMainClass.java
@@ -10,7 +10,13 @@ public class JCheckBoxMainClass extends JFrame {
 		JCheckBox jcb2=new JCheckBox("장기회원",true);
 		JCheckBox jcb3=new JCheckBox("해외여행신청여부", false);
 		
-	}
+		jp1.add(jcb1);
+		jp1.add(jcb2);
+		jp1.add(jcb3);
+		getContentPane().add(jp1);
+		//프레임의 contentpane을 알아낸후 컴포넌트 부착
+		//프레임=메뉴바+contentPane(컴포넌트 붙이는 곳)
+	}//생성자 end
 
 	public static void main(String[] args) {
 		JCheckBoxMainClass jcbc1 = new JCheckBoxMainClass();
