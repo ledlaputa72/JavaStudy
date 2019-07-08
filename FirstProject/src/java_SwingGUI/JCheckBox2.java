22e3539b94a7251bf27c3bba2bde80c75d2918fa
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-08 15:18:25 +0900

    add

--- FirstProject/src/java_SwingGUI/JCheckBox2.java
+++ FirstProject/src/java_SwingGUI/JCheckBox2.java
@@ -1,12 +1,12 @@
 package java_SwingGUI;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
