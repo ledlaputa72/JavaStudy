5f0d0ebe78912ab45d1f357e715fec8a53a5f77c
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-18 11:19:31 +0900

    add

---
+++ FirstProject/src/Day044_Thread/RunnableMainClass3.java
@@ -0,0 +1,43 @@
+package Day044_Thread;
+
+class RunnableClass10 implements Runnable{
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("»ç¶ûÇÕ´Ï´Ù.");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class RunnableClass20 implements Runnable{
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Ich Liebe Dich");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class RunnableMainClass3 {
+
+	public static void main(String[] args) {
+		RunnableClass10 rc10=new RunnableClass10();  
+		Thread th10=new Thread(rc10);
+		
+		RunnableClass20 rc20=new RunnableClass20();  
+		Thread th20=new Thread(rc20);
+		
+		th10.start();
+		th20.start();
+	}
+}
