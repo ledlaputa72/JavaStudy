ce5cabe31d98c48dd79332c2b7cf1a36197efdea
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-18 10:00:40 +0900

    add

--- FirstProject/src/java_Thread01/ThreadMainClass6.java
+++ FirstProject/src/java_Thread01/ThreadMainClass6.java
@@ -1,61 +1,82 @@
 package java_Thread01;
 
-class CutomerInfo {
-	static String name="Jone";
-	static int sum=100;
+class CustomerInfo {
+	private String name;
+	private int sum;
+	//생성자
+	public CustomerInfo(String name, int sum) {
+		this.name = name;
+		this.sum = sum;
+	}
 	
-	public static String getName() {
+	public CustomerInfo(int sum) {
+		this.sum = sum;
+	}
+
+	//getter setter
+	public String getName() {
 		return name;
 	}
-	public static void setName(String name) {
-		CutomerInfo.name = name;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	public static int getSum() {
+
+	public int getSum() {
 		return sum;
 	}
-	public static void setSum(int sum) {
-		CutomerInfo.sum = sum;
+
+	public void setSum(int sum) {
+		this.sum = sum;
 	}
 	
-	public void Account(String name, int sum) {
-		System.out.println(name+" + "+sum);
+	//출금
+	public int chool(int sum) {
+		this.sum -= sum;
+		System.out.println(name+"님의 출금 -----금액 : "+sum);
+		return sum;
 	}
-	
-	public void Output(String name, int sum) {
-		System.out.println(name+" ---------- "+ 200);
-		sum = sum -200;
+	//입금
+	public int ip(int janaek) {
+		this.sum += janaek;
+		System.out.println(name+"님의 출금 +++++금액 : "+janaek);
+		return janaek;
 	}
-	
+
+	@Override
+	public String toString() {
+		return name+"님의 잔액은 "+getSum() + "원입니다.";
+	}
 	
 }//고객 정보 클래스 
 
 //1번 손님 
 class ThreadBank1 extends Thread{
-	CutomerInfo cf1=new CutomerInfo();
 	
 	public void run() {
 		System.out.println("1st 손님 입장 "); 
-		cf1.setName("Tom");
-		cf1.setSum(500);
-		cf1.Account(cf1.getName(),cf1.getSum());
-		cf1.Output(cf1.getName(),cf1.getSum());
-		cf1.Account(cf1.getName(),cf1.getSum());
-
-		
+		CustomerInfo cf1=new CustomerInfo("정연섭",500); //객체 생성이 run()안에서 
+		System.out.println(cf1);
+		cf1.chool(300);
+		System.out.println(cf1);
+		cf1.ip(200);
+		System.out.println(cf1);
+		System.out.println(cf1.getName()+"님이 나갑니다.");
 	}
 }//1번 손님 클래스 
 
 // 2번 손님 
 class ThreadBank2 extends Thread{
-	CutomerInfo cf2=new CutomerInfo();
-	
+
 	public void run() {
 		System.out.println("2nd 손님 입장 ");
-		cf2.setName("Jay");
-		cf2.setSum(1000);
-		cf2.Account(cf2.getName(),cf2.getSum());
-		cf2.Output(cf2.getName(),cf2.getSum());
-		cf2.Account(cf2.getName(),cf2.getSum());
+		CustomerInfo cf2=new CustomerInfo("정제이",300);
+		System.out.println(cf2);
+		cf2.chool(500);
+		System.out.println(cf2);
+		cf2.ip(100);
+		System.out.println(cf2);
+		System.out.println(cf2.getName()+"님이 나갑니다.");
 	}
 	
 }//2번 손님 클래스 
@@ -63,7 +84,6 @@ class ThreadBank2 extends Thread{
 public class ThreadMainClass6 {
 
 	public static void main(String[] args) {
-		CutomerInfo ci1=new CutomerInfo();
 		
 		ThreadBank1 tb1=new ThreadBank1();
 		ThreadBank2 tb2=new ThreadBank2();
@@ -74,3 +94,4 @@ public class ThreadMainClass6 {
 	}
 
 }
+
