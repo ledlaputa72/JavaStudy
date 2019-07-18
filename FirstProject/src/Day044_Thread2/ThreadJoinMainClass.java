5b53d48cbf6599dafa877b1f14730580a595f1b4
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-18 15:09:48 +0900

    add

---
+++ FirstProject/src/Day044_Thread2/ThreadJoinMainClass.java
@@ -0,0 +1,28 @@
+package Day044_Thread2;
+
+class ThreadJoinClass extends Thread{
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("쓰레드 시작");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}//run end
+}
+
+public class ThreadJoinMainClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		ThreadJoinClass tcj1=new ThreadJoinClass();
+		tcj1.start();
+		System.out.println("나는 스레드메인");
+		tcj1.join();
+		System.out.println("쓰레드");
+	}
+
+}
