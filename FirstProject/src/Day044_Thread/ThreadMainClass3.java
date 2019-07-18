73ed634cdc56a2515fa541b20dd519f4ce93d31f
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-18 13:49:27 +0900

    add

--- FirstProject/src/Day044_Thread/ThreadMainClass3.java
+++ FirstProject/src/Day044_Thread/ThreadMainClass3.java
@@ -1,10 +1,20 @@
 package Day044_Thread;
 
+class TheadClass3 implements Runnable{
+
+	@Override
+	public void run() {
+		while(true) { //公茄风橇 
+			System.out.println("公茄风橇");
+		}
+	}
+}
+
 public class ThreadMainClass3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
+		Thread tr30=new Thread(new TheadClass3());
 	}
 
 }
