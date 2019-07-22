935096c1273f335e1e2911c8043766ef771da1a8
ledlaputa <ledlaputa@DESKTOP-2QT66QE>
2019-07-23 01:35:20 +0900

    add

--- FirstProject/src/Day046_Tcp_dis1_thread_1vs1/TcpClient.java
+++ FirstProject/src/Day046_Tcp_dis1_thread_1vs1/TcpClient.java
@@ -2,6 +2,7 @@ package Day046_Tcp_dis1_thread_1vs1;
 
 import java.net.*;
 import java.io.*;
+import Day046_Tcp_dis1.TcpServer.*;
 
 public class TcpClient {
 	public static void main(String args[]) throws UnknownHostException, IOException {
