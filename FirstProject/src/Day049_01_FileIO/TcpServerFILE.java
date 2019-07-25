2eaa442b1673425187b6386af12a0ced656d7e02
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-25 10:47:51 +0900

    add

---
+++ FirstProject/src/Day049_01_FileIO/TcpServerFILE.java
@@ -0,0 +1,16 @@
+package Day049_01_FileIO;
+
+import java.io.*;
+import java.net.*;
+
+public class TcpServerFILE {
+
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket ss1=new ServerSocket(37788);
+		System.out.println("서버 준비되어있음.....");
+		
+		
+	}
+
+}
