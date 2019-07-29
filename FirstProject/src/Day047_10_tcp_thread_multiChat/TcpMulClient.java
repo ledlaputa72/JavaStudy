3a6724afa1651866809dfd8365d340102257c77d
network5096 <46048095+network5096@users.noreply.github.com>
2019-07-29 15:18:58 +0900

    add

--- FirstProject/src/Day047_10_tcp_thread_multiChat/TcpMulClient.java
+++ FirstProject/src/Day047_10_tcp_thread_multiChat/TcpMulClient.java
@@ -1,11 +1,20 @@
 package Day047_10_tcp_thread_multiChat;
 
+import java.awt.Color;
+import java.awt.EventQueue;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+//Client Send Class/////////////////////////////////////////////
 class ThreadClientSendClass extends Thread {
 	
 	Socket socket;
@@ -42,15 +51,19 @@ class ThreadClientSendClass extends Thread {
 		
 }
 	
+//Client RCV Class////////////////////////////////////////
 class ThreadClientRcvClass extends Thread {
 
 	Socket socket;
 	DataInputStream inputStream;
+	JTextArea textArea;
+
 	
 	// 생성자
-	public ThreadClientRcvClass(Socket socket) throws IOException {
+	public ThreadClientRcvClass(Socket socket, JTextArea textArea ) throws IOException {
 		
 		this.socket = socket;
+		this.textArea =  textArea;
 		
 		inputStream = new DataInputStream(socket.getInputStream()); // read
 		
@@ -63,6 +76,10 @@ class ThreadClientRcvClass extends Thread {
 			try {
 				
 				System.out.println(inputStream.readUTF());
+				//textArea로 보내주기 ///////////////////////////////////////////
+				
+				textArea.setText(inputStream.readUTF());
+				
 				
 			} catch (IOException e) {
 				
@@ -73,25 +90,35 @@ class ThreadClientRcvClass extends Thread {
 
 }
 
-public class TcpMulClient {
-
+//main class
+public class TcpMulClient extends JFrame{
+	
+	private JPanel contentPane;
+	static JTextField textField;
+	static JTextArea textArea; 
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		if (args.length != 3) {
+		/*if (args.length != 3) {
 			
 			System.out.println("사용법 : 클라이언트실행은 \'java 패키지명.파일명 ip주소 포트번호 닉네임 \' 형식으로 입력");
 			
 			System.exit(1);
-		}
+		}*/
+		TcpMulClient jfmc = new TcpMulClient();
+		jfmc.setVisible(true);
+		
 			
 		try {
+
 			
-			Socket s1 = new Socket(args[0], Integer.parseInt(args[1])); // ip 주소, 포트 번호
+//			Socket s1 = new Socket(args[0], Integer.parseInt(args[1])); // ip 주소, 포트 번호
+			Socket s1 = new Socket("127.0.0.1", 9999); // ip 주소, 포트 번호
 			
 			System.out.println("서버에 연결...");
 			
-			ThreadClientSendClass tcc1 = new ThreadClientSendClass(s1, args[2]); // 닉네임
-			ThreadClientRcvClass tcr1 = new ThreadClientRcvClass(s1);
+			ThreadClientSendClass tcc1 = new ThreadClientSendClass(s1, "Test"); // 닉네임
+			ThreadClientRcvClass tcr1 = new ThreadClientRcvClass(s1,textArea);
 			
 			Thread tsend1 = new Thread(tcc1); // 보내는 chat 위해
 			Thread trcv1 = new Thread(tcr1); // 받는 chat 위해
@@ -103,6 +130,43 @@ public class TcpMulClient {
 			
 			e.printStackTrace();
 		}
-	}
-
+	}//main end
+	
+	//생성자 /////////////////////////////////////////////////
+	public TcpMulClient() {
+		
+			JPanel panelTop = new JPanel();
+			panelTop.setBackground(Color.CYAN);
+			panelTop.setBounds(0, 0, 1184, 60);
+			contentPane.add(panelTop);
+			
+			JPanel panelCanvas = new JPanel();
+			panelCanvas.setBackground(Color.LIGHT_GRAY);
+			panelCanvas.setBounds(0, 60, 800, 702);
+			contentPane.add(panelCanvas);
+			
+			JPanel panelChat = new JPanel();
+			panelChat.setBackground(Color.GRAY);
+			panelChat.setBounds(800, 60, 384, 702);
+			contentPane.add(panelChat);
+			panelChat.setLayout(null);
+			
+			//////////////////////////////////////////
+			
+			textField = new JTextField();
+			textField.setBounds(0, 664, 384, 38);
+			panelChat.add(textField);
+			textField.setColumns(10);
+			
+			textArea = new JTextArea();
+			textArea.setBounds(0, 0, 384, 664);
+			panelChat.add(textArea);
+			////////////////////////////////////////////
+			
+			JPanel panelUsers = new JPanel();
+			panelUsers.setBackground(Color.ORANGE);
+			panelUsers.setBounds(0, 759, 1184, 180);
+			contentPane.add(panelUsers);
+		}//생성자 디폴트 
 }
+
--- FirstProject/src/Day047_10_tcp_thread_multiChat/TcpMulServer.java
+++ FirstProject/src/Day047_10_tcp_thread_multiChat/TcpMulServer.java
@@ -121,11 +121,12 @@ public class TcpMulServer {
 
 	public static void main(String[] args) throws IOException {
 
-		if (args.length != 1) 
-			System.out.println("사용법 : 서버실행은 \'java 패키지명.파일명 포트번호 \' 형식으로 입력");
+		/*if (args.length != 1) 
+			System.out.println("사용법 : 서버실행은 \'java 패키지명.파일명 포트번호 \' 형식으로 입력");*/
 		
 		// 객체 생성 - 초기치 생성자
-		new ServerClass(Integer.parseInt(args[0])); 
+//		new ServerClass(Integer.parseInt(args[0]));
+		new ServerClass(7890); 
 		
 	}
 
