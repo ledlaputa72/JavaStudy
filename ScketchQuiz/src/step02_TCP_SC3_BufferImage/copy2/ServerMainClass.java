a8e687ce326367a69f6450f826a11ec39751dfff
Ledlaputa <Ledlaputa@172.30.49.189>
2019-08-01 01:12:16 +0900

    addd

--- ScketchQuiz/src/step02_TCP_SC3_BufferImage/copy2/ServerMainClass.java
+++ ScketchQuiz/src/step02_TCP_SC3_BufferImage/copy2/ServerMainClass.java
@@ -30,23 +30,25 @@ class ServerRcvSend extends Thread {
 	
 	BufferedImage bufferImage1=null;
 	BufferedInputStream bis1;
+	
 	BufferedImage bufferImage2=null;
 	BufferedOutputStream bos1;
 	
 	public ServerRcvSend(Socket s1, ServerFrame sf1) throws IOException {
 		this.s1 = s1;
 		this.sf1 = sf1;
-		// 받아오기 ///////////////////////////////////////
+
 		bis1 = new BufferedInputStream(s1.getInputStream());
 		bos1 = new BufferedOutputStream(s1.getOutputStream());
 		
 		bufferImage1= ImageIO.read(bis1); //받은 데이터를 버퍼에 저장
 		System.out.println("받은 이미지 : " + bufferImage1); // 버퍼내용 확인
 		while (true) {
-			//받기 
+			//받기 먼저#######################################
 			sf1.frame.getGraphics().drawImage(ImageIO.read(ImageIO.createImageInputStream(bis1)), 0, 0, 800, 400, sf1.frame);
-			System.out.println("받은 이미지 : " + bis1);
-			//보내기 
+			System.out.println("받은 이미지 :  " + bis1);
+			
+			//보내기 다음######################################
 			ImageIO.write(bufferImage1, "bmp", bos1);// 그 이미지를 png파일로 소켓 아웃풋스트림으로 쏴줌
 			bos1.flush(); // 버퍼에 쓰인 이미지를 서버로 보
 			System.out.println("보내는 이미지 : " + bos1);
@@ -62,9 +64,8 @@ public class ServerMainClass {
 		ServerFrame sf1 = new ServerFrame();
 		sf1.setVisible(true);
 
-		ServerSocket ss1 = new ServerSocket(9999);
+		ServerSocket ss1 = new ServerSocket(8888);
 		System.out.println("서버 대기중 ....");
-		
 
 		while (true) {
 			Socket s1 = ss1.accept();
@@ -72,7 +73,7 @@ public class ServerMainClass {
 			//쓰레드 /////////////////////////////
 			ServerRcvSend srs1 = new ServerRcvSend(s1,sf1);
 			srs1.start();
-			
+			System.out.println("쓰레드밖 ");
 
 		}
 	}
