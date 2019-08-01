21999c442096e0e3ac77918e070d124528ba010d
network5096 <46048095+network5096@users.noreply.github.com>
2019-08-01 15:18:09 +0900

    add

--- ScketchQuiz/src/Day047_01_TCP_Multi/TcpMulServer.java
+++ ScketchQuiz/src/Day047_01_TCP_Multi/TcpMulServer.java
@@ -1,11 +1,10 @@
-package step03_Day047_01_TCP_Multi;
+package Day047_01_TCP_Multi;
 
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import javax.imageio.ImageIO;
 
+import Day045_Thread3_Network.IPMainClass;
 
 //////////////////////////////////////////////////////////
 class ServerClass {
@@ -17,7 +16,7 @@ class ServerClass {
 	Socket socket; //클라이언트 받아 저장할 곳
 	DataOutputStream outputStream; //출력 스트림 
 	
-	public ServerClass(int portno) throws IOException, InterruptedException { //생성자 
+	public ServerClass(int portno) throws IOException { //생성자 
 		Socket s1 = null;
 		ServerSocket ss1 = new ServerSocket(portno); //서버소켓 생성 Listen 
 		System.out.println("서버가동......");
@@ -25,14 +24,11 @@ class ServerClass {
 		while (true ) {
 			s1 = ss1.accept(); //기다리는 중 
 			System.out.println("접속주소 : "+ s1.getInetAddress() + ", 접속포트: "+s1.getPort());
-			//#############################################################
-			ServerRcvSend serverRS = new ServerRcvSend(s1);//s1초기치 
+			//===================================================
 			ThreadServerClass tServer1 = new ThreadServerClass(s1);//s1초기치 
-			serverRS.start();
 			tServer1.start();
-			
 			threadList.add(tServer1);//컬랙션에 add
-			//#############################################################
+			//=====================================================
 			System.out.println("접속자 수 : " + threadList.size()+" 명");
 		}// while end
 	}// 생성자 end
@@ -46,9 +42,7 @@ class ServerClass {
 		
 	}
 	
-	//#########################################
-	//##    쓰레드 채팅 클래스                                                 ##
-	//#########################################
+	//////////////////////////////////////////////////
 	class ThreadServerClass extends Thread {
 		Socket socket1;
 		DataInputStream inputStream;
@@ -95,66 +89,17 @@ class ServerClass {
 			} // finally end
 		} //run end
 	}// ThreadServerClass end
-	
-	//#########################################
-	//##    쓰레드 이미지 통신 클래스                                       ##
-	//#########################################
-	class ServerRcvSend extends Thread {
-		Socket s1;
-		 
-		BufferedInputStream bufferInput;
-		BufferedOutputStream bufferOutput;
-		
-		BufferedImage bufferImage1; //추가)
-		BufferedImage bufferImage2;
-		 
-		public ServerRcvSend(Socket s1) throws IOException, InterruptedException {
-			this.s1 = s1;
-			bufferInput = new BufferedInputStream(s1.getInputStream());
-			bufferOutput = new BufferedOutputStream(s1.getOutputStream());
-		}//생성자
-	
-		public void run() { //run
-			try{
-				while (true) {
-					bufferImage1= ImageIO.read(bufferInput); //추가) 받은 데이터를 버퍼에 저장
-					System.out.println("SR1) 받은 이미지 버퍼: " + bufferImage1); //추가) 버퍼내용 확인
-					//받기 먼저#######################################
-					System.out.println("1111################################################");
-					System.out.println("SR2) 받은 이미지 스트림 : " + bufferInput); //추가) 받은 스트림 데이터 확인 
-					System.out.println("22222################################################");
-					
-					
-					//버퍼에 저장############################################
-					bufferImage2=bufferImage1;
-					System.out.println("버퍼1 :  " + bufferImage1);
-					System.out.println("버퍼2 :  " + bufferImage2 );
-					
-					
-					//보내기##############################################
-					System.out.println("SS1) 버퍼2 :  " + bufferImage2 );
-		
-					ImageIO.write(bufferImage2, "bmp", bufferOutput);// 그 이미지를 png파일로 소켓 아웃풋스트림으로 쏴줌
-					System.out.println("SS2) 보내는  이미지 버퍼 : " + bufferImage2);
-					bufferOutput.flush(); // 버퍼에 쓰인 이미지를 서버로 보
-					System.out.println("SS3) 보내는 이미지 : " + bufferOutput);
-				
-				}//while end
-			}
-			catch(Exception e) {
-				System.out.println("ServerRcvSend 에러내용 : " + e);
-			}
-		}//run end
-	}//ServerRcv end
 }// ServerClass end
-
 
 public class TcpMulServer {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException {
+		if(args.length != 1) {
+			System.out.println("사용법 : 서버실행은 "+" \'java 패키지명.파일명 포트번호\' 형식으로 입력");
+		}
 		
 		//new ServeClass()
-		new ServerClass(9999);
+		new ServerClass(Integer.parseInt(args[0]));
 		//객체생성 - 초기치 생성자 
 	}
 
--- ScketchQuiz/src/step03_Day047_01_TCP_Multi/TcpMulClient.java
+++ ScketchQuiz/src/step03_Day047_01_TCP_Multi/TcpMulClient.java
@@ -1,10 +1,156 @@
 package step03_Day047_01_TCP_Multi;
 
+import java.awt.AWTException;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+class TestFrameClass extends JFrame implements MouseListener, MouseMotionListener, ActionListener  {
+	JPanel contentPane; //메인 컨텐츠페인
+	static JPanel panelCanvas; //그림 그리는 패널
+	static JPanel panelView; //그림 그리는 패널
+	static int panelCanvasX;
+	static int panelCanvasY;
+	
+	int w = Toolkit.getDefaultToolkit().getScreenSize().width, h = Toolkit.getDefaultToolkit().getScreenSize().height;
+	int witchiX=0, witchiY=0; //마우스의 좌표 , 클래스 밑에 생긴 전역 변수
+	
+	//그리기 옵션용 컴포넌트
+	Color bgColor=Color.WHITE; //background color
+	Color brColor=Color.BLUE; //bruch color
+	int brSize=10; //brush Size
+	int sw=0; //배경색 sw=1, 붓색 sw=2
+	
+	public TestFrameClass() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1200, 900);
+		setResizable(false);
+		setAlwaysOnTop(true);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		panelCanvas = new JPanel();
+		panelCanvas.setBackground(Color.YELLOW);
+		panelCanvas.setBounds(0, 60, 800, 300);
+		panelCanvas.setVisible(true);
+		contentPane.add(panelCanvas);//추가
+		
+		panelView = new JPanel();
+		panelView.setBackground(Color.RED);
+		panelView.setBounds(0, 460, 800, 300);
+		panelView.setVisible(true);
+		contentPane.add(panelView);//추가
+		
+		panelCanvas.addMouseListener(this); //중요!!!! 마우스 이벤트 등록
+		panelCanvas.addMouseMotionListener(this); //중요!!! 마우스 액션 이벤트 등록
+		}
+
+	
+	
+	public JPanel getPanelCanvas() {
+		return panelCanvas;
+	}
+
+	public void setPanelCanvas(JPanel panelCanvas) {
+		this.panelCanvas = panelCanvas;
+	}
+
+	public JPanel getPanelView() {
+		return panelView;
+	}
+
+	public void setPanelView(JPanel panelView) {
+		this.panelView = panelView;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		Graphics g=panelCanvas.getGraphics(); //중요!!! 그래픽의 객체를 만듬 (그림을 그릴 대상에)
+		g.setColor(brColor); //칼라를 설정 
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setStroke(new BasicStroke(brSize));
+		
+		//whichiX,whichiY : 처음위치 
+		int badaX=e.getX(); //방금(끝) 마우스 위치 
+		int badaY=e.getY();
+		
+		g.drawLine(witchiX, witchiY, badaX, badaY); //처음 -> 끝 위치까지 선 그리기
+		witchiX=badaX; //다시 처음 위치 값에 끝 값을 넣는다. 
+		witchiY=badaY;
+		//다시 위의 것을 반복한다. 
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		witchiX=e.getX(); //마우스X의 현재 위치 
+		witchiY=e.getY(); //마우스Y의 현재 위치 
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}//TestFrameClass end
+
 
 class ThreadClientSendClass extends Thread {
 	
@@ -73,31 +219,104 @@ class ThreadClientRcvClass extends Thread {
 
 }
 
+
+//#########################################################
+//보내기 쓰레드 ////////////////////////////////////////
+class ThreadSend extends Thread{
+	
+	static final int w = 800, h = 700; 
+	static final int x = Toolkit.getDefaultToolkit().getScreenSize().width / 2 - w / 2, y = Toolkit.getDefaultToolkit().getScreenSize().height / 2 - h / 2;
+
+	Socket s1;
+	Robot robot;
+	static BufferedImage bufferImage1;
+	static BufferedOutputStream bos1;
+	
+	TestFrameClass mainFrame;
+	
+	public ThreadSend(Socket s1, TestFrameClass mainFrame) throws IOException, AWTException {
+		this.s1 = s1;
+		this.mainFrame = mainFrame;		
+		
+		robot = new Robot();
+		
+		while(true) {
+			//보내기 ######################################################
+			bos1 = new BufferedOutputStream(s1.getOutputStream()); 
+			
+			//좌표 핵심##################################################################
+			TestFrameClass.panelCanvasX=(int)TestFrameClass.panelCanvas.getLocationOnScreen().getX(); //좌표 변수를 만들고  static으로 변경
+			TestFrameClass.panelCanvasY=(int)TestFrameClass.panelCanvas.getLocationOnScreen().getY();
+			//###########################################################################
+			bufferImage1 = robot.createScreenCapture(new Rectangle(TestFrameClass.panelCanvasX-4, TestFrameClass.panelCanvasY-60, 800, 700));//스크린샷을 찍어서 image에 저장해
+			//image = robot.createScreenCapture(new Rectangle(0, 60, 800, 300));//스크린샷을 찍어서 image에 저장해
+			System.out.println("CS1) 보내는 이미지 : " + bufferImage1);
+			
+			ImageIO.write(bufferImage1, "bmp", bos1);//그 이미지를 png파일로 소켓 아웃풋스트림으로 쏴줌
+			System.out.println("CS2) 보내는 이미지 : " + bos1);
+			
+			bos1.flush(); //버퍼에 쓰인 이미지를 서버로 보냄
+			System.out.println("CS3) 보내는 이미지 : " + bos1);
+		}
+	}
+}////////////////////////////////////////
+
+//#########################################################
+//받기 쓰레드 ////////////////////////////////////////
+class ThreadRcv extends Thread{
+	
+	Socket s1;
+	static BufferedImage bufferImage2;
+	static BufferedInputStream bis1;
+	TestFrameClass mainFrame;
+	
+	public ThreadRcv(Socket s1, TestFrameClass mainFrame) throws IOException, AWTException {
+		this.s1 = s1;
+		this.mainFrame = mainFrame;
+
+		while(true) {
+			bis1 = new BufferedInputStream(s1.getInputStream());
+			System.out.println("CR1) 받은 이미지 : " + bis1);
+			
+			bufferImage2=ImageIO.read(bis1); //받은 이미지를 버퍼에
+			System.out.println("CR2) 받은 이미지 : " + bufferImage2);
+			
+			mainFrame.panelView.getGraphics().drawImage(ImageIO.read(ImageIO.createImageInputStream(bis1)), 0, 360, 800, 400, mainFrame.panelView);
+			System.out.println("CR3) 받은 이미지 : " + bis1);
+		}
+	}
+}/////////////////////////////////////////////
+
 public class TcpMulClient {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 
-		if (args.length != 3) {
-			
-			System.out.println("사용법 : 클라이언트실행은 \'java 패키지명.파일명 ip주소 포트번호 닉네임 \' 형식으로 입력");
-			
-			System.exit(1);
-		}
-			
 		try {
-			
-			Socket s1 = new Socket(args[0], Integer.parseInt(args[1])); // ip 주소, 포트 번호
-			
+			Socket s1 = new Socket("127.0.0.1", 9999); // ip 주소, 포트 번호
 			System.out.println("서버에 연결...");
 			
-			ThreadClientSendClass tcc1 = new ThreadClientSendClass(s1, args[2]); // 닉네임
-			ThreadClientRcvClass tcr1 = new ThreadClientRcvClass(s1);
+			TestFrameClass mainFrame=new TestFrameClass(); //그림 그리기 작동
+			mainFrame.setVisible(true);
 			
-			Thread tsend1 = new Thread(tcc1); // 보내는 chat 위해
+			ThreadClientSendClass tcc1 = new ThreadClientSendClass(s1, "Test"); // 닉네임
+			ThreadClientRcvClass tcr1 = new ThreadClientRcvClass(s1);
+			ThreadSend ts1=new ThreadSend(s1,mainFrame);
+			ThreadRcv tr1=new ThreadRcv(s1,mainFrame);
+			
+			/*Thread tsend1 = new Thread(tcc1); // 보내는 chat 위해
 			Thread trcv1 = new Thread(tcr1); // 받는 chat 위해
+			Thread tsend2 = new Thread(ts1); // 보내는 이미지 위해
+			Thread trcv2 = new Thread(tr1); // 받는 이미지 위해
 			
 			tsend1.start();
 			trcv1.start();
+			tsend2.start();
+			trcv2.start();*/
+			
+			tcc1.start();
+			tcr1.start();
+			ts1.start();
+			tr1.start();
 			
 		} catch (Exception e) {
 			
