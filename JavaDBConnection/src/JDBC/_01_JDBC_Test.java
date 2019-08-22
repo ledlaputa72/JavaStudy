d9732642b5a5066d0af06d93c689588317952d2a
network5096 <46048095+network5096@users.noreply.github.com>
2019-08-22 15:16:33 +0900

    add

--- JavaDBConnection/src/JDBC/_01_JDBC_Test.java
+++ JavaDBConnection/src/JDBC/_01_JDBC_Test.java
@@ -10,11 +10,9 @@ public class _01_JDBC_Test {
 		// TODO Auto-generated method stub
 		Connection con1=null;
 		
-		Class.forName ("oracle.jdbc.driver.OracleDreiver");
+		Class.forName ("oracle.jdbc.driver.OracleDriver");
 		con1 = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:" + "1521:xe", "hr", "hr"); //db에 접속
 		//("jdbc:oracle:thin:@127.0.0.1:1521:xe", "hr", "hr");
 		System.out.println("접속했다고 전해라");
-
 	}
-
 }
