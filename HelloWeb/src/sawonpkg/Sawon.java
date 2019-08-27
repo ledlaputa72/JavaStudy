8bccdba52dea3b987a37a453aeebf9d6a196dff4
network5096 <46048095+network5096@users.noreply.github.com>
2019-08-27 13:19:22 +0900

    add

---
+++ HelloWeb/src/sawonpkg/Sawon.java
@@ -0,0 +1,24 @@
+package sawonpkg;
+
+public class Sawon {
+	
+	private String irum;
+	private String na2;
+	
+	//인자없는 생성자, getter setter 생성 
+	public Sawon() { } 
+	
+	public String getIrum() {
+		return irum;
+	}
+	public void setIrum(String irum) {
+		this.irum = irum;
+	}
+	public String getNa2() {
+		return na2;
+	}
+	public void setNa2(String na2) {
+		this.na2 = na2;
+	}
+
+}
